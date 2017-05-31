381109174207b4216482b5cf8533483951f1e838
Rohan Kamat <rohan.ka@media.net>
2017-05-31 16:56:26 +0530

    Add command line args --script and --gulp and add eslint

--- index.js
+++ index.js
@@ -1,188 +1,188 @@
-const { spawn, spawnSync } = require('child_process');
+const { spawn } = require('child_process');
 const inquirer = require('inquirer');
+const minimist = require('minimist');
 const fs = require('fs');
+
 const { log: l } = console;
 
-const [execPath, execFile, argPath = '', defaultProject = '', defaultModule = '', defaultTask = ''] = process.argv;
+const argv = minimist(process.argv.slice(2));
+const [argPath = '', defaultProject = '', defaultModule = ''] = argv._;
+const { gulp = null, script = null } = argv;
 
 const currentDirectory = `${process.cwd()}`;
 const rootDirectory = ((/^\//g).test(argPath)) ? argPath : currentDirectory + argPath;
 
 const portfinder = require('portfinder');
 
-const getProjectPath = (project) => `${rootDirectory}/${project}`;
+const getProjectPath = project => `${rootDirectory}/${project}`;
 
 const getModulePath = (project, module) => `${getProjectPath(project)}/public/${module}`;
 
 if (!fs.existsSync(rootDirectory)) {
-    l(`Directory not found!`);
-    process.exit(1);
+  l('Directory not found!');
+  process.exit(1);
 }
 
 
-const hasPackageJson = (path) => {
-    return fs.existsSync(path + '/package.json');
-}
+const hasPackageJson = path => fs.existsSync(`${path}/package.json`);
 
-const isGulp = (path) => {
-    return fs.existsSync(`${path}/Gulpfile.js`) || fs.existsSync(`${path}/gulpfile.js`);
-};
+const isGulp = path => fs.existsSync(`${path}/Gulpfile.js`) || fs.existsSync(`${path}/gulpfile.js`);
 
 
+const validate = path => isGulp(path) || hasPackageJson(path);
 
-const validate = (path) => {
-    return isGulp(path) || hasPackageJson(path);
-}
-
-const findModules = (project) => {
-    return fs.existsSync(`${rootDirectory}/${project}/public`) &&
+const findModules = project => fs.existsSync(`${rootDirectory}/${project}/public`) &&
         fs.readdirSync(`${rootDirectory}/${project}/public`).filter(
             dir => (/^.+-app$/).test(dir) && fs.statSync(getModulePath(project, dir)).isDirectory() && validate(getModulePath(project, dir)));
 
-};
-
-const projects = fs.readdirSync(rootDirectory).filter(dir => {
-    return fs.statSync(getProjectPath(dir)).isDirectory() && fs.existsSync(`${getProjectPath(dir)}/artisan`) && findModules(dir).length
-});
+const projects = fs.readdirSync(rootDirectory).filter(dir => fs.statSync(getProjectPath(dir)).isDirectory() && fs.existsSync(`${getProjectPath(dir)}/artisan`) && findModules(dir).length);
 
 if (!projects.length) {
-    l('No Projects found');
-    process.exit(1);
+  l('No Projects found');
+  process.exit(1);
 }
-
 
 
 const getNpmScripts = (project, module) => {
-    const { scripts } = require(`${getModulePath(project, module)}/package.json`);
-    const { start: s = null } = scripts;
+  const { scripts } = require(`${getModulePath(project, module)}/package.json`);  // eslint-disable-line
+  const { start: st = null } = scripts;
 
-    const start = s ? 'start' : ''
+  const start = st ? 'start' : '';
 
-    return [start, ...Object.keys(scripts).filter(script => script !== 'start')].filter(x => x);
-}
-
-const getAllMatches = (regex, str) => {
-    let m;
-    const result = [];
-    while ((m = regex.exec(str)) !== null) {
-        // This is necessary to avoid infinite loops with zero-width matches
-        if (m.index === regex.lastIndex) {
-            regex.lastIndex++;
-        }
-
-        result.push(m);
-    }
-
-    return result;
-}
-
-const getGulpTasks = (project, module) => {
-    var gulpFileContent = fs.readFileSync(`${getModulePath(project, module)}/Gulpfile.js`, "utf8");
-    const matches = getAllMatches(/gulp\.task\(\'(?!default\b)\b(\w+)\'/g, gulpFileContent).map(r => r[1]).filter(x => x);
-    return ['default', ...matches];
+  return [start, ...Object.keys(scripts).filter(s => s !== 'start')].filter(x => x);
 };
 
+const getAllMatches = (regex, str) => {
+  let m;
+  const result = [];
+  while ((m = regex.exec(str)) !== null) {  // eslint-disable-line no-cond-assign
+    if (m.index === regex.lastIndex) {
+      regex.lastIndex += 1;
+    }
+
+    result.push(m);
+  }
+
+  return result;
+};
+
+const getGulpTasks = (project, module) => {
+  const gulpFileContent = fs.readFileSync(`${getModulePath(project, module)}/Gulpfile.js`, 'utf8');
+  const matches = getAllMatches(/gulp\.task\('(?!default\b)\b(\w+)'/g, gulpFileContent).map(r => r[1]).filter(x => x);
+  return ['default', ...matches];
+};
+
+const defaultAnswer = {};
 
 const questions = [{
-    type: 'list',
-    name: 'project',
-    message: 'Select Project',
-    choices: projects,
-    when: (answers) => {
-        if (defaultProject) {
-            answers.project = defaultProject;
-            return false;
-        }
-        return true;
+  type: 'list',
+  name: 'project',
+  message: 'Select Project',
+  choices: projects,
+  when: (answers) => {
+    if (defaultProject) {
+      defaultAnswer.project = defaultProject;
+      answers.project = defaultProject;
+      return false;
     }
+    return true;
+  },
 }, {
-    type: 'list',
-    name: 'module',
-    message: 'Select Module',
-    choices: (answers) => {
-        return findModules(answers.project);
-    },
-    when: (answers) => {
-        if (defaultModule) {
-            answers.module = defaultModule;
-            return false;
-        }
-        return findModules(answers.project);
-    },
-}, {
-    type: 'confirm',
-    name: 'startArtOnly',
-    message: 'No Modules found, Do you want to start artisan?',
-    default: true,
-    when: (answers) => {
-        return !findModules(answers.project);
-    },
-}, {
-    type: 'list',
-    name: 'gulpTask',
-    message: 'Select the gulp task to run',
-    choices: (answers) => {
-        return getGulpTasks(answers.project, answers.module);
-    },
-    when: (answers) => {
-        return isGulp(getModulePath(answers.project, answers.module));
-    },
-}, {
-    type: 'list',
-    name: 'scripts',
-    message: 'Select the npm script to run',
-    choices: ({ project, module }) => {
-        l(project, module);
-        return getNpmScripts(project, module);
-    },
-    when: (answers) => {
-        return findModules(answers.project) && !isGulp(answers.project, answers.module);
+  type: 'list',
+  name: 'module',
+  message: 'Select Module',
+  choices: answers => findModules(answers.project),
+  when: (answers) => {
+    if (defaultModule) {
+      defaultAnswer.module = defaultModule;
+      answers.module = defaultModule;
+      return false;
     }
+    return findModules(answers.project);
+  },
+}, {
+  type: 'confirm',
+  name: 'startArtOnly',
+  message: 'No Modules found, Do you want to start artisan?',
+  default: true,
+  when: answers => !findModules(answers.project),
+}, {
+  type: 'list',
+  name: 'gulpTask',
+  message: 'Select the gulp task to run',
+  choices: (answers) => {
+    if (gulp) {
+      defaultAnswer.gulp = gulp;
+      answers.gulp = gulp;
+      return false;
+    }
+    return getGulpTasks(answers.project, answers.module);
+  },
+  when: answers => isGulp(getModulePath(answers.project, answers.module)),
+}, {
+  type: 'list',
+  name: 'scripts',
+  message: 'Select the npm script to run',
+  choices: ({ project, module }) => {
+    l(project, module);
+    return getNpmScripts(project, module);
+  },
+  when: (answers) => {
+    if (script) {
+      defaultAnswer.scripts = script;
+      answers.scripts = script;
+      return false;
+    }
+
+    return findModules(answers.project) && !isGulp(answers.project, answers.module);
+  },
 }];
 
 
+inquirer.prompt(questions).then((answers) => {
+  const gulpTask = answers.gulpTask || defaultAnswer.gulpTask;
+  const module = answers.module || defaultAnswer.module;
+  const project = answers.project || defaultAnswer.project;
+  const scripts = answers.scripts || defaultAnswer.scripts;
 
-inquirer.prompt(questions).then(function(answers) {
-
-    const { gulpTask, module, project, startArtOnly = false, scripts } = answers;
-
-    l(JSON.stringify(answers, null, '  '));
+  l(JSON.stringify(answers, null, '  '));
 
 
-    let proc = null
-    portfinder.getPortPromise().then(port => {
-        l('Starting artisan serve');
-        const proc = spawn('php', ['artisan', 'serve', '--port=' + port], { stdio: [0, 1, 2], cwd: getProjectPath(project) });
-        proc.on('message', (m) => l(m));
-        proc.on('exit', (code) => {
-            if (code === 0) {
-                l('Artisan started on port ' + port);
-            } else {
-                process.exit(1);
-            }
-        });
-        proc.on('error', () => {
-            process.exit(1);
-        })
-        l("Artisan running as " + proc.pid);
+  let proc = null;
+  portfinder.getPortPromise().then((port) => {
+    l('Starting artisan serve');
+
+    proc = spawn('php', ['artisan', 'serve', `--port=${port}`], { stdio: [0, 1, 2], cwd: getProjectPath(project) });
+    proc.on('message', m => l(m));
+    proc.on('exit', (code) => {
+      if (code === 0) {
+        l(`Artisan started on port ${port}`);
+      } else {
+        process.exit(1);
+      }
     });
+    proc.on('error', (...args) => {
+      l(args);
+      process.exit(1);
+    });
+    l(`Artisan running as ${proc.pid}`);
+  });
 
-    let nProc = null;
+  let nProc = null;
 
-    if (gulpTask) {
-        l('Starting gulp...');
-        nProc = spawn('gulp', [gulpTask], { stdio: [0, 1, 2], cwd: getModulePath(project, module) });
-    } else if (scripts) {
-        l('Starting npm run ' + scripts + '...');
-        nProc = spawn('npm', ['run', scripts], { stdio: [0, 1, 2], cwd: getModulePath(project, module) });
-    }
+  if (gulpTask) {
+    l('Starting gulp...');
+    nProc = spawn('gulp', [gulpTask], { stdio: [0, 1, 2], cwd: getModulePath(project, module) });
+  } else if (scripts) {
+    l(`Starting npm run ${scripts}...`);
+    nProc = spawn('npm', ['run', scripts], { stdio: [0, 1, 2], cwd: getModulePath(project, module) });
+  }
 
-    const cleanUp = (...args) => {
-        l('killing');
-        nProc && nProc.kill();
-        proc && proc.kill();
-    };
+  const cleanUp = () => {
+    l('killing');
+    nProc && nProc.kill(); // eslint-disable-line no-unused-expressions
+    proc && proc.kill();   // eslint-disable-line no-unused-expressions
+  };
 
-    process.on('SIGINT', cleanUp);
-
+  process.on('SIGINT', cleanUp);
 });
